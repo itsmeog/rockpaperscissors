91203772391a24f983f3c7716bf70f6be9568649
itsmeog <149211510+itsmeog@users.noreply.github.com>
2023-12-11 19:09:49 +0100

    add win-loss cases

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ function getComputerChoice() {
   let random = choice[Math.floor(Math.random() * choice.length)];
   return random;
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return ("It's a draw!");
+  } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+    return ("You win! Rock beats scissors");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return ("You lose! Paper beats rock");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return ("You win! Paper beats rock");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return ("You lose! Scissors beat paper");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return ("You win! Scissors beat paper");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return ("You lose!Rock beats scissors");
+  }
+}
+ 
+const playerSelection = prompt("What do you choose?");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
